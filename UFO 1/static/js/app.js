fc1da9621efb6cbd00a1fa131c46f8dfdb5f2792
Ken <kensikes988@gmail.com>
2021-05-15 09:19:46 -0700

    saving

--- UFO 1/static/js/app.js
+++ UFO 1/static/js/app.js
@@ -5,7 +5,7 @@ var tableData = data;
 // YOUR CODE HERE!
 var button = d3.select("#filter-btn");
 
-// Get a reference to the table body
+// Reference to the table body
 var tbody = d3.select("tbody");
 
 function displayTable(datafortable) {
@@ -18,3 +18,23 @@ function displayTable(datafortable) {
     })
 };
 
+button.on("click", function() {
+
+      var inputValue = d3.select("#datetime").property("value");
+  
+    console.log(inputValue);
+    console.log(d3.event.target.value);
+
+    if (inputValue !== "") {
+       
+        var filteredData = tableData.filter(date => date.datetime === inputValue);
+        console.log(filteredData);
+    
+              tbody.html("");
+        displayTable(filteredData);
+    }
+    else {
+        displayTable(data);
+    }
+
+});
