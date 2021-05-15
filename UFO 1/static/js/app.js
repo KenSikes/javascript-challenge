b221aefcd05811ef4d040caaca944eccad5b0266
Ken <kensikes988@gmail.com>
2021-05-15 08:56:39 -0700

    saving app.js

--- UFO 1/static/js/app.js
+++ UFO 1/static/js/app.js
@@ -1,4 +1,20 @@
+  
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+var button = d3.select("#filter-btn");
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+function displayTable(datafortable) {
+    datafortable.forEach((tableData) => {
+        var row = tbody.append("tr");
+        Object.entries(tableData).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    })
+};
+
