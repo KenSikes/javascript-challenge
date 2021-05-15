a6ea84400d8f2090a85e939624905c7573277d0e
Ken <kensikes988@gmail.com>
2021-05-15 09:55:17 -0700

    saving

--- UFO 2/static/js/app.js
+++ UFO 2/static/js/app.js
@@ -7,7 +7,7 @@ var button = d3.select("#filter-btn");
 
 var button = d3.select("#filter-btn");
 
-// Reference to the table body
+
 var tbody = d3.select("tbody");
 
 function displayTable(datafortable) {
@@ -26,3 +26,11 @@ displayTable(tableData);
 
 button.on("click", function() {
     var filteredData = tableData;
+    
+        var inputDate = d3.select("#datetime").property("value");
+        var inputCity = d3.select("#city").property("value").toLowerCase();
+        var inputState = d3.select("#state").property("value").toLowerCase();
+        var inputCountry = d3.select("#country").property("value").toLowerCase();
+        var inputShape = d3.select("#shape").property("value").toLowerCase();
+
+        
